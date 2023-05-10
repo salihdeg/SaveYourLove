6c76ad0582173a1f0381036c8ea6a05aaa9b3db8
salihdeg <salihdegirmenci99@gmail.com>
2023-05-10 16:17:20 +0300

    Attack proccess begin

--- Assets/Scripts/Player/PlayerAttack.cs
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -6,13 +6,20 @@ namespace Player
 {
     public class PlayerAttack : MonoBehaviour
     {
+        [SerializeField] private Transform _attackPoint;
+        [SerializeField] private Transform _attackPoint2;
+        [SerializeField] private float _attackRange = 0.5f;
+        [SerializeField] private LayerMask _enemyLayer;
+
         private Animator _animator;
+        private Rigidbody2D _rb;
 
         [HideInInspector] public bool isAttacking = false;
 
         private void Awake()
         {
             _animator = GetComponentInChildren<Animator>();
+            _rb = GetComponent<Rigidbody2D>();
         }
 
         private void Start()
@@ -24,8 +31,7 @@ namespace Player
         {
             if (Input.GetMouseButtonDown(0))
             {
-                isAttacking = true;
-                _animator.SetTrigger(AnimationStates.isAttacking);
+                Attack();
             }
 
 
@@ -36,8 +42,13 @@ namespace Player
         private void Attack()
         {
             // Play an attack anim
+            isAttacking = true;
+            _animator.SetTrigger(AnimationStates.isAttacking);
+            _rb.velocity = new Vector2(0f, _rb.velocity.y);
             // Detect enemies in range
+            Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(_attackPoint.position, _attackRange, _enemyLayer);
             // Damage them
         }
     }
+
 }
