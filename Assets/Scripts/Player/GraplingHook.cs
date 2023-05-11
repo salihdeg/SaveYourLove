b6684efd433bbd453ab8816959491ec7c09d872d
salihdeg <salihdegirmenci99@gmail.com>
2023-05-11 16:09:26 +0300

    New feature added

---
+++ Assets/Scripts/Player/GraplingHook.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GraplingHook : MonoBehaviour
+{
+    private Camera _camera;
+    private LineRenderer _lineRenderer;
+    private DistanceJoint2D _distanceJoint;
+
+
+}
